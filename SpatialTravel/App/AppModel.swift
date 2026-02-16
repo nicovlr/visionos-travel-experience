777433cb02596aee935332855d57524037c925a0
nicolas valerino <159495253+nicovlr@users.noreply.github.com>
2026-02-16 13:16:58 +0100

    Improve AppModel with dependency injection and loading states

--- SpatialTravel/App/AppModel.swift
+++ SpatialTravel/App/AppModel.swift
@@ -3,16 +3,55 @@ import SwiftUI
 @Observable
 class AppModel {
     let immersiveSpaceID = "ImmersiveSpace"
+
     enum ImmersiveSpaceState {
         case closed
         case inTransition
         case open
     }
-    var immersiveSpaceState = ImmersiveSpaceState.closed
 
+    enum LoadingState {
+        case idle
+        case loading
+        case loaded
+        case error(String)
+    }
+
+    var immersiveSpaceState = ImmersiveSpaceState.closed
     var selectedDestination: Destination?
     var isShowingDetail = false
+    var loadingState = LoadingState.idle
+    var destinations: [Destination] = []
 
-    // TODO: move this to a proper service
-    var destinations: [Destination] = Destination.samples
+    private let service: DestinationServiceProtocol
+
+    init(service: DestinationServiceProtocol = DestinationService()) {
+        self.service = service
+    }
+
+    // MARK: - Data Loading
+
+    func loadDestinations() async {
+        guard case .idle = loadingState else { return }
+        loadingState = .loading
+
+        do {
+            destinations = try await service.fetchDestinations()
+            loadingState = .loaded
+        } catch {
+            loadingState = .error(error.localizedDescription)
+        }
+    }
+
+    // MARK: - Selection
+
+    func selectDestination(_ destination: Destination) {
+        selectedDestination = destination
+        isShowingDetail = true
+    }
+
+    func clearSelection() {
+        selectedDestination = nil
+        isShowingDetail = false
+    }
 }
